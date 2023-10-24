4587ac7986ad9728ae1f36a72cb8fecdf97bb22b
ASDleves <ricsi20020302@gmail.com>
2023-10-24 08:08:47 +0200

    merge

--- mvc/Controller/Controller.js
+++ mvc/Controller/Controller.js
@@ -5,28 +5,6 @@ class Controller {
     constructor() {
         this.urlapModel = new UrlapModel();
         this.urlapView = new UrlapView($(".urlap"), this.urlapModel.leiro);
-
-        jQuery(this.urlapView.submitElem).on("click", (event) => {
-            event.preventDefault();
-            let isValid = true;
-            let urlapAdatok = {};
-
-            this.urlapView.urlapElemList.forEach((elem) => {
-                isValid = isValid && elem.valid;
-            });
-
-            if (isValid) {
-                console.log("Valid az űrlap!");
-                this.urlapView.urlapElemList.forEach((elem) => {
-                    let ertek = elem.ertek;
-                    let kulcs = elem.key;
-                    urlapAdatok[kulcs] = ertek;
-                });
-                console.log(urlapAdatok);
-            } else {
-                console.log("Nem valid az űrlap!");
-            }
-        });
     }
 }
 
